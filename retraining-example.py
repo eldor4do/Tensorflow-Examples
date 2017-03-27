8cb7c0aba765a18fe89946974d22213d856b9bce
Srikanth Anantharam <sria91@gmail.com>
2017-03-27 11:56:56 +0530

    `w` is already a string

--- retraining-example.py
+++ retraining-example.py
@@ -72,7 +72,7 @@ def run_inference_on_image():
         top_k = predictions.argsort()[-5:][::-1]  # Getting top 5 predictions
         f = open(labelsFullPath, 'r')
         lines = f.readlines()
-        labels = [str(w).replace("\n", "") for w in lines]
+        labels = [w.replace("\n", "") for w in lines]
         for node_id in top_k:
             human_string = labels[node_id]
             score = predictions[node_id]
